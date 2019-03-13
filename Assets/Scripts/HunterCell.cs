d4e855ae940a8537b397a31dcccf190f3784c567
TheQuestCrafter <thequestcrafter@gmail.com>
2019-03-13 03:45:51 -0500

    Cinemachine and Basic Movement

--- Assets/Scripts/HunterCell.cs
+++ Assets/Scripts/HunterCell.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HunterCell : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private Rigidbody2D rb2d;
+    [SerializeField]
+    private float slowCoefficient = 2;
+
+    private float directionX;
+    private float directionY;
+
+    void FixedUpdate()
     {
-        
+        Move(); 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Move()
     {
-        
+        //Moves player on input
+        directionX = Input.GetAxis("Horizontal");
+        Vector2 vector = new Vector2(moveSpeed, moveSpeed);
+        if (!Input.GetKeyDown(KeyCode.A) || !Input.GetKey(KeyCode.D))
+        {
+            vector.x = (vector.x / slowCoefficient);
+            vector.x = vector.x * directionX;
+        }
+
+        directionY = Input.GetAxis("Vertical");
+        if (!Input.GetKeyDown(KeyCode.W) || !Input.GetKey(KeyCode.S))
+        {
+            vector.y = (vector.y / slowCoefficient);
+            vector.y = vector.y * directionY;
+        }
+        rb2d.velocity = (vector);
     }
 }
